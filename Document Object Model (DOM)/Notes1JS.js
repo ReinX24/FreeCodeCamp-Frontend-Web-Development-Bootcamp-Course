a08688de74ef8462be3f84faf6b7814dbb06006f
rein <reinX244@gmail.com>
2023-07-16 19:15:36 +0800

    Finisehd Document Object Model (DOM) chapter. Personal exercises to be done.

--- Document Object Model (DOM)/Notes1JS.js
+++ Document Object Model (DOM)/Notes1JS.js
@@ -42,6 +42,21 @@ function addParagraph() {
 // btn.onclick = addParagraph; // another way of registering a click event
 
 const myList = document.querySelector('ul');
-console.log(myList);
-console.log(myList.nodeType); // prints noteType 1, check documentation for more info
-console.log(myList.children) // childer of ul element, li elements
+// console.log(myList);
+// console.log(myList.nodeType); // prints noteType 1, it means that it is an element
+// console.log(myList.children) // childer of ul element, li elements
+// console.log(myList.children.item(1)); // prtins second li in ul
+
+const listItem = myList.children.item(1);
+listItem.textContent = 'some new content'; // changes second li item contents
+
+myList.appendChild(document.createElement('li')); // adding another li element
+myList.children.item(3).textContent = 'some more content'; // setting li element text
+
+// Adding an element between 2 elements
+// document.querySelector('p'); // only gets first instance
+const secondPElement = document.querySelectorAll('p').item(1); // gets second instance of p element
+const parentPElement = document.querySelector('.main-body'); // gets parent element of p, the main-body div
+const newP = document.createElement('p'); // creating a new p element
+newP.textContent = 'programmatically added between'; // setting p element text
+parentPElement.insertBefore(newP, secondPElement);
