db626ba0ea24a2e7408afdbba797a464eadd2c83
rein <reinX244@gmail.com>
2023-11-01 14:10:59 +0800

    Finished Fetch API Introduction and started Create a Custom PDF Viewer with JavaScript tutorial.

--- Fetch API Introduction/script.js
+++ Fetch API Introduction/script.js
@@ -1,6 +1,7 @@
 document.querySelector('#getText').addEventListener('click', getText);
 document.querySelector('#getUsers').addEventListener('click', getUsers);
 document.querySelector('#getPosts').addEventListener('click', getPosts);
+document.querySelector('#addPost').addEventListener('click', addPost);
 
 function getText(e) {
     // console.log(e.target); // prints the element that has the event listener
@@ -36,14 +37,14 @@ function getUsers() {
 
             const outputDiv = document.querySelector('#output');
 
-            let outputContent = '<h2>Users</h2>';
+            let outputContent = '<h2 class="mb-4">Users</h2>';
 
             data.forEach((eachData) => {
                 outputContent += `
-                    <ul>
-                    <li>ID: ${eachData.id}</li>
-                    <li>Name: ${eachData.name}</li>
-                    <li>Email: ${eachData.email}</li>
+                    <ul class="list-group mb-3">
+                    <li class="list-group-item">ID: ${eachData.id}</li>
+                    <li class="list-group-item">Name: ${eachData.name}</li>
+                    <li class="list-group-item">Email: ${eachData.email}</li>
                     </ul>
                    `
             })
@@ -58,11 +59,11 @@ function getPosts() {
 
             const outputDiv = document.querySelector('#output');
 
-            let outputPosts = '<h2>Posts</h2>'
+            let outputPosts = '<h2 class="mb-4">Posts</h2>'
 
             posts.forEach((eachPost) => {
                 outputPosts += `
-                <div>
+                <div class="card card-body mb-3">
                     <h3>${eachPost.title}</h3>
                     <p>${eachPost.body}</p>
                 </div>
@@ -72,4 +73,24 @@ function getPosts() {
             outputDiv.innerHTML = outputPosts;
 
         })
+}
+
+function addPost(e) {
+    e.preventDefault();
+
+    const title = document.querySelector('#title').value;
+    const body = document.querySelector('#body').value;
+
+    // API link, object
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json, text/plain, */*',
+            'Content-type': 'application/json'
+        },
+        body: JSON.stringify({ title: title, body: body })
+    })
+        .then((res) => res.json())
+        .then((data) => console.log(data))
+
 }
