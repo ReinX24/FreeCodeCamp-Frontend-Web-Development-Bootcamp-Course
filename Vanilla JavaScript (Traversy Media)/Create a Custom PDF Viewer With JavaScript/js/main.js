ca6fa23c85e75736bf81469e611f9cd80829bde2
rein <reinX244@gmail.com>
2023-11-02 21:44:01 +0800

    PDF now shows in the browser but still cannot be traversed.

--- Vanilla JavaScript (Traversy Media)/Create a Custom PDF Viewer With JavaScript/js/main.js
+++ Vanilla JavaScript (Traversy Media)/Create a Custom PDF Viewer With JavaScript/js/main.js
@@ -11,7 +11,61 @@ const scale = 1.5,
 
 // Render the page
 const renderPage = (pageNum) => {
+    pageIsRendering = true;
+
+    // Get the page
+    pdfDoc.getPage(pageNum).then(currentPage => {
+        // Set scale
+        const viewPort = currentPage.getViewport({ scale })
+        canvas.height = viewPort.height;
+        canvas.width = viewPort.width;
+
+        // For rendering the pdf on the screen
+        const renderContext = {
+            canvasContext: ctx,
+            viewport: viewPort
+        };
+
+        // Rendering the current page and additional pages
+        currentPage.render(renderContext).promise.then(() => {
+            pageIsRendering = false;
+
+            if (pageNumIsPending !== null) {
+                renderPage(pageNumIsPending);
+                pageNumIsPending = null;
+            }
+        })
+
+        // Output current page
+        document.querySelector('#page-num').textContent = pageNum;
+    });
 
 }
 
+// Check for pages rendering
+const queueRenderPage = (currentPageNumber) => {
+    if (pageIsRendering) {
+        pageNumIsPending = currentPageNumber;
+    } else {
+        renderPage(currentPageNumber)
+    }
+}
+
+// Show Prev Page
+const showPrevPage = () => {
+    // If the pageNum is less than or equal than 1, do nothing
+    if (pageNum <= 1) {
+        return;
+    }
+    pageNum--;
+    queueRenderPage(pageNum);
+}
+
 // Get document
+pdfjsLib.getDocument(url).promise.then(loadedPdf => {
+    pdfDoc = loadedPdf;
+
+    document.querySelector('#page-count').textContent = pdfDoc.numPages;
+
+    renderPage(pageNum);
+});
