f45297b9f1ff6187bc802d3a47ac3c1ad84dda08
rein <reinX244@gmail.com>
2023-11-07 22:13:50 +0800

    Added matching functionality for entered text and state name and abbreviation.

--- Vanilla JavaScript (Traversy Media)/Quick Autocomplete App With JS & JSON/js/main.js
+++ Vanilla JavaScript (Traversy Media)/Quick Autocomplete App With JS & JSON/js/main.js
@@ -5,8 +5,18 @@ const matchList = document.querySelector('#match-list');
 const searchStates = async (searchText) => {
     const res = await fetch('data/states.json');
     const states = await res.json();
-    console.log(states);
-    // Resume link: https://youtu.be/1iysNUrI3lw?si=guIyCR67pcJkui7G&t=606
+
+    // Get matches to current text input (searchText)
+    let matches = states.filter((eachState) => {
+        // ^ means starts with searchText, g means global, i means ignore case
+        const regex = new RegExp(`^${searchText}`, 'gi');
+        return eachState.name.match(regex) || eachState.abbr.match(regex);
+    });
+
+    if (searchText == 0) {
+        matches = [];
+    }
+    console.log(matches)
 }
 
-search.addEventListener('input', searchStates(search.value));
+search.addEventListener('input', () => searchStates(search.value));
