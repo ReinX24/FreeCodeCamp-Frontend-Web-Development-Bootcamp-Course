50a71ddec1321e022dd306f31abde3523b8751ff
rein <reinX244@gmail.com>
2024-02-19 20:57:34 +0800

    Finished psr_autoloading_composer, also added class_constants.

---
+++ php-the-right-way/2_class_constants/src/public/index.php
@@ -0,0 +1,5 @@
+<?php
+
+require __DIR__ . '/../vendor/autoload.php';
+
+use App\PaymentGateway\Paddle\Transaction; // triggers autoloader
--- php-the-right-way/2_psr_autoloading_composer/src/public/index.php
+++ php-the-right-way/2_psr_autoloading_composer/src/public/index.php
@@ -1,12 +1,27 @@
 <?php
 
-require_once '../app/PaymentGateway/Stripe/Transaction.php';
-require_once '../app/PaymentGateway/Paddle/CustomerProfile.php';
-require_once '../app/PaymentGateway/Paddle/Transaction.php';
-require_once '../app/Notification/Email.php';
+// require_once '../app/PaymentGateway/Stripe/Transaction.php';
+// require_once '../app/PaymentGateway/Paddle/CustomerProfile.php';
+// require_once '../app/PaymentGateway/Paddle/Transaction.php';
+// require_once '../app/Notification/Email.php';
 
-use App\PaymentGateway\Paddle\Transaction;
+// Checks for namespaces required by the program
+// spl_autoload_register(function ($class) {
+//     $path = __DIR__ . '/../' .  lcfirst(str_replace('\\', '/', $class)) . '.php';
+
+//     if (file_exists($path)) {
+//         require $path;
+//     }
+// });
+
+require __DIR__ . '/../vendor/autoload.php';
+
+use App\PaymentGateway\Paddle\Transaction; // triggers autoloader
 
 $paddleTransaction = new Transaction();
 
 var_dump($paddleTransaction);
+
+$id = new \Ramsey\Uuid\UuidFactory();
+
+echo $id->uuid4();
