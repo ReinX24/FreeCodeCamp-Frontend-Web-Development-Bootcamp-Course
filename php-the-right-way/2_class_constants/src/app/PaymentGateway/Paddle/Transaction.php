090496b04d0f2d0743ed9f2579ed786b4c3f871e
rein <reinX244@gmail.com>
2024-02-24 20:10:24 +0800

    Finished class_constants and started php-for-beginners-laracasts.

--- php-the-right-way/2_class_constants/src/app/PaymentGateway/Paddle/Transaction.php
+++ php-the-right-way/2_class_constants/src/app/PaymentGateway/Paddle/Transaction.php
@@ -4,10 +4,31 @@ declare(strict_types=1);
 
 namespace App\PaymentGateway\Paddle;
 
+use App\Enums\Status;
 
 class Transaction
 {
+    private string $status;
+
     public function __construct()
     {
+        // var_dump(Transaction::STATUS_PAID);
+        // var_dump(self::STATUS_PAID); // self - current class
+
+        // $this->setStatus('pending');
+        // $this->setStatus(self::STATUS_PENDING);
+
+        $this->setStatus(Status::PENDING);
+    }
+
+    public function setStatus(string $status):self {
+        // Sets new status string and returns an object with updated attributes
+        // Checks if the $status parameter is part of the stored valid statuses
+        if (!isset(Status::ALL_STATUSES[$status])) {
+            throw new \InvalidArgumentException;
+        }
+        $this->status = $status;
+
+        return $this;
     }
 }
--- php-the-right-way/2_class_constants/src/public/index.php
+++ php-the-right-way/2_class_constants/src/public/index.php
@@ -3,3 +3,15 @@
 require __DIR__ . '/../vendor/autoload.php';
 
 use App\PaymentGateway\Paddle\Transaction; // triggers autoloader
+use App\Enums\Status;
+
+$transaction = new Transaction();
+
+// $transaction->setStatus('paid');
+// $transaction->setStatus(Transaction::STATUS_PAID);
+// $transaction->setStatus('awjhdioahjiofj');
+$transaction->setStatus(Status::PAID);
+
+var_dump($transaction);
+
+// echo Transaction::class;
