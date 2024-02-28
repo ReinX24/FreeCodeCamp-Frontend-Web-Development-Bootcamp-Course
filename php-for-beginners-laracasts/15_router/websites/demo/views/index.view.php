83619f98eb7a0ebaab71ce733514bd0ed15077d5
rein <reinX244@gmail.com>
2024-02-28 21:14:59 +0800

    Finished partials and superglobals, currently on router.

--- php-for-beginners-laracasts/15_router/websites/demo/views/index.view.php
+++ php-for-beginners-laracasts/15_router/websites/demo/views/index.view.php
@@ -7,7 +7,7 @@
 <main>
     <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
         <!-- Your content -->
-        <p>Hello. Welcome to the contact page.</p>
+        <p>Hello. Welcome to the home page.</p>
     </div>
 </main>
 
