fec5ab18733fca854a059ec7695862fd88b16e34
rein <reinX244@gmail.com>
2024-03-04 22:46:52 +0800

    Finished notes_page and math_functions, started if_statements.

--- bro-code-php/5_math_functions/index.php
+++ bro-code-php/5_math_functions/index.php
@@ -9,30 +9,65 @@
 
 <body>
     <form action="index.php" method="POST">
-        <label for="x">x:</label>
-        <input type="text" name="x">
+        <label for="radius">Radius:</label><br>
+        <input type="text" name="radius"><br>
 
-        <label for="y">x:</label>
-        <input type="text" name="y">
+        <input type="submit" value="Calculate">
+    </form>
+    <br>
+
+    <!-- <form action="index.php" method="POST">
+        <label for="x">x:</label>
+        <input type="text" name="x"><br>
+
+        <label for="y">y:</label>
+        <input type="text" name="y"><br>
+
+        <label for="z">z:</label>
+        <input type="text" name="z"><br>
 
         <input type="submit" value="Total">
-    </form>
+    </form> -->
 </body>
 
 </html>
 
 <?php
 
-$x = $_POST['x'];
-$y = $_POST['y'];
-$total = null;
+$radius = $_POST['radius'];
+
+$circumference = null;
+$area = null;
+$volume = null;
+
+$circumference = 2 * pi() * $radius;
+$circumference = round($circumference, 2);
+
+$area = pi() * pow($radius, 2);
+$area = round($area, 2);
+
+$volume = (4 / 3) * pi() * pow($radius, 3);
+$volume = round($volume, 2);
+
+echo "Circumference = {$circumference}cm<br>";
+echo "Area = {$area}cm^2 <br>";
+echo "Volume = {$volume}cm^3 <br>";
+
+// $x = $_POST['x'];
+// $y = $_POST['y'];
+// $z = $_POST['z'];
+// $total = null;
 
 // $total = abs($x);
 // $total = round($x);
 // $total = floor($x);
 // $total = ceil($x);
-$total = pow($x, $y);
+// $total = pow($x, $y);
+// $total = max($x, $y, $z);
+// $total = min($x, $y, $z);
+// $total = pi();
+// $total = rand(90, 100); // random number between 2 numbers (incl. both)
 
-echo $total;
+// echo $total;
 
 ?>
---
+++ bro-code-php/6_if_statements/index.php
@@ -0,0 +1,7 @@
+<?php
+
+$age = 21;
+
+if ($age >= 18) {
+    echo "You may enter this site";
+}
